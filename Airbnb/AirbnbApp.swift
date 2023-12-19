0616d1132874a616b92efee2beb02dbb01b88cca
MayramAlymkulova <mayram.a2003@gmail.com>
2023-12-19 19:29:47 +0600

    Initial Commit

---
+++ Airbnb/AirbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbApp.swift
+//  Airbnb
+//
+//  Created by A Mairam on 19/12/23.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
